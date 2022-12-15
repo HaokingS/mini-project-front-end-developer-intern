0512cf94cab9473ab87a556c88c97b3c9da22aa4
Haoking Suryanatmaja <109867904+HaokingS@users.noreply.github.com>
2022-12-15 17:31:26 +0700

    Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import './App.css';
 import HelloWorld from './Components/HelloWorld';
-import CounterExample from './Components/CounterExample';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return(
     <div>
-      <CounterExample />
+      <Header />
+
+      <HelloWorld name = "Haoking"/>
+
+      <Footer />
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/Components/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
